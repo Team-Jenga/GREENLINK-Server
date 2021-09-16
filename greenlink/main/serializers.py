9393ac837811c72066f3161c610fce9617db3a18
Choiyunh_ <svs101@naver.com>
2021-09-16 17:40:28 +0900

    serializers

--- greenlink/main/serializers.py
+++ greenlink/main/serializers.py
@@ -9,3 +9,33 @@ class MainSerializer(serializers.ModelSerializer):
             'content',
         )
         model = Main
+
+class MemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'member_id',
+            'member_pw',
+            'member_name',
+            'member_nickname',
+        )
+        model = Main
+
+class MemberAdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'member_id',
+            'member_admin_position',
+        )
+        model = Main
+
+class MemberUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'member_id',
+            'member_user_birth',
+            'member_user_phone',
+            'member_user_email',
+            'member_user_location',
+            'member_user_number_of_family',
+        )
+        model = Main
