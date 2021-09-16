b666200efb57a7265b9b1f8f74dd8042955ef46a
Choiyunh_ <svs101@naver.com>
2021-09-16 17:40:21 +0900

    Update admin.py

--- greenlink/main/admin.py
+++ greenlink/main/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Main
+from .models import Main, Member, MemberAdmin, MemberUser
 
 admin.site.register(Main)
+admin.site.register(Member)
+admin.site.register(MemberAdmin)
+admin.site.register(MemberUser)
