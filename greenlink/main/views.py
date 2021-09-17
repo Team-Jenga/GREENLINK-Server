4dbbac67f3f896fa48615dcd881cc958b2c8c028
Choiyunh_ <svs101@naver.com>
2021-09-18 00:29:11 +0900

    로그인 API 또 수정

--- greenlink/main/views.py
+++ greenlink/main/views.py
@@ -83,7 +83,7 @@ class SignIn(View):
                 user = Member.objects.get(member_id = data['member_id'])
 
                 if bcrypt.checkpw(data['member_pw'].encode('UTF-8'), user.member_pw.encode('UTF-8')):
-                    token = jwt.JWT().encode({'user' : user.member_id}, SECRET_KEY, algorithm = 'HS256').decode('UTF-8')
+                    token = jwt.encode({'member_id' : user.member_id}, SECRET_KEY, 'HS256').decode('UTF-8')
 
                     return JsonResponse({"message" : "Success !"}, {'token' : token}, status=200)
 
